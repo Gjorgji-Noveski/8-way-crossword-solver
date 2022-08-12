e6c4b44ca89e8f5e76dc1eba60a13bb14237422e
Gjorgi Noveski <g.noveski@yahoo.com>
2022-08-12 15:22:40 +0200

    WIP

--- imagePreprocessing.py
+++ imagePreprocessing.py
@@ -13,19 +13,25 @@ def preproces_image(img_path):
     blurred_img = cv2.blur(img_resized, (5, 5))
     hist_equal = cv2.equalizeHist(blurred_img)
 
-    # thresh_img = cv2.bitwise_not(hist_equal) THIS OR THIS BELOW
 
-    thresh_img = cv2.adaptiveThreshold(hist_equal, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 25, 30)
+    thresh_img = cv2.adaptiveThreshold(hist_equal, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 25, 15)
+
+    # We are doing this because for morphological operators, opencv considers the white color as foreground if we don't
+    # do this, our morphological operators will be flipped (erode will do dilation, and vice versa)
+    thresh_img = cv2.bitwise_not(thresh_img)
 
     elipse_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
     # morph open
-    morphed_elipse = cv2.morphologyEx(thresh_img, cv2.MORPH_OPEN, elipse_kernel, iterations=8)
+    morphed_elipse = cv2.morphologyEx(thresh_img, cv2.MORPH_OPEN, elipse_kernel, iterations=1)
 
     # morph close
-    morphed_elipse_close_inv = cv2.morphologyEx(morphed_elipse, cv2.MORPH_CLOSE, elipse_kernel, iterations=8)
+    morphed_elipse_close_inv = cv2.morphologyEx(morphed_elipse, cv2.MORPH_CLOSE, elipse_kernel, iterations=1)
 
-    # morph erode
-    # morphed_elipse_close_inv1 = cv2.morphologyEx(morphed_elipse_close_inv, cv2.MORPH_ERODE, elipse_kernel, iterations=8)
+    # morph dilate
+    morphed_elipse_close_inv1 = cv2.morphologyEx(morphed_elipse_close_inv, cv2.MORPH_DILATE, elipse_kernel, iterations=1)
+
+    # to make letters black again
+    thresh_img = cv2.bitwise_not(morphed_elipse_close_inv1)
 
     cv2.imwrite('processed_image.jpg', thresh_img)
