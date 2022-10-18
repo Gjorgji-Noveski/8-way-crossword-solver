213e8c340b6b704a38905e9c08600506a02f6a2f
Gjorgji-Noveski <g.noveski@yahoo.com>
2022-10-19 00:08:39 +0200

    Massively changed layout

--- GUI.py
+++ GUI.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap,QFont
+from PyQt5.QtGui import QPixmap, QFont
 from PyQt5.QtWidgets import *
 
 from imagePreprocessing import ImgPreprocessing
@@ -28,43 +28,74 @@ class CrosswordSolver(QWidget):
         self.layout.addWidget(self)
         self.scrollArea = None
 
+        # Layouts
+        self.subLayout1h = QHBoxLayout(self)
+        self.subLayout1hChild1 = QVBoxLayout(self)
+        self.subLayout1hChild2 = QVBoxLayout(self)
+        self.subLayout1h.addLayout(self.subLayout1hChild1)
+        separatorLine = QFrame()
+        separatorLine.setFrameShape(QFrame.VLine)
+        separatorLine.setSizePolicy(QSizePolicy.Minimum,QSizePolicy.Expanding)
+        separatorLine.setLineWidth(3)
+
+        self.subLayout1h.addWidget(separatorLine)
+        self.subLayout1h.addLayout(self.subLayout1hChild2)
+        self.subLayout1hChild1.setAlignment(Qt.AlignLeft)
+        self.subLayout1hChild2.setAlignment(Qt.AlignLeft)
+
+        self.subLayout1h.setStretch(1, 1)  # This fixes the layout in place, so the widgets don't move
+
+
+
+        self.subLayout2h = QHBoxLayout(self)
+        self.subLayout2hChild1 = QVBoxLayout(self)
+        self.subLayout2hChild2 = QVBoxLayout(self)
+        self.subLayout2h.addLayout(self.subLayout2hChild1)
+        self.subLayout2h.addLayout(self.subLayout2hChild2)
+
+        self.subLayout3h = QHBoxLayout(self)
+
+        self.layout.addLayout(self.subLayout1h)
+        self.layout.addLayout(self.subLayout2h)
+        self.layout.addLayout(self.subLayout3h)
+
         # Language button
         self.languageLabel = QLabel(self)
         self.languageLabel.setText("What is the language of the letters in the word grid?")
-        self.layout.addWidget(self.languageLabel)
+        self.subLayout1hChild1.addWidget(self.languageLabel)
         self.languageSelectBox = QComboBox(self)
         self.languageSelectBox.setFixedWidth(int(screen.availableSize().width() * 0.08))  # 10$ of the screen's width
         self.languageSelectBox.addItem("Macedonian (MK)", "mkd")
         self.languageSelectBox.addItem("English (EN)", "eng")
         self.languageSelectBox.currentIndexChanged.connect(self.rerunOcr)
-        self.layout.addWidget(self.languageSelectBox)
+        self.subLayout1hChild1.addWidget(self.languageSelectBox)
 
-        self.layout.addWidget(self.imageHolder)
+        self.subLayout2hChild1.addWidget(self.imageHolder)
 
         chooseCrosswordPicBtn = QPushButton("Choose picture")
-        self.layout.addWidget(chooseCrosswordPicBtn)
+        self.subLayout2hChild1.addWidget(chooseCrosswordPicBtn)
         chooseCrosswordPicBtn.clicked.connect(self.dialogSelectImage)
 
         # Input text widget
         self.textBox = PlainTextEdit(self)
         self.textBox.setPlaceholderText("Insert search words separated by space, ex: tree cat sky")
         self.textBox.setMaximumHeight(200)
-        self.layout.addWidget(self.textBox)
+        self.subLayout2hChild2.addWidget(self.textBox)
 
         # Search Words button
         self.searchWordsBtn = QPushButton("Search words")
         self.searchWordsBtn.setDisabled(True)
-        self.layout.addWidget(self.searchWordsBtn)
+        self.subLayout2hChild2.addWidget(self.searchWordsBtn)
         self.searchWordsBtn.clicked.connect(self.searchForWords)
 
         # Columns in word word field
         self.columnLabel = QLabel(self)
         self.columnLabel.setText("How many columns does the word grid have?")
-        self.layout.addWidget(self.columnLabel)
+        self.subLayout1hChild2.addWidget(self.columnLabel)
         self.columnsField = QSpinBox(self)
         self.columnsField.setValue(10)
         self.columnsField.setMaximumWidth(int(screen.availableSize().width() * 0.04))
-        self.layout.addWidget(self.columnsField)
+        self.subLayout1hChild2.addWidget(self.columnsField)
 
         # Displaying results
         self.resultLabel = QLabel(self)
@@ -73,8 +104,8 @@ class CrosswordSolver(QWidget):
         self.resultLabel.setAlignment(Qt.AlignLeft | Qt.AlignTop)
         self.resultsLabelInfoText = QLabel(self)
         self.resultsLabelInfoText.setText("Results from search are:")
-        self.layout.addWidget(self.resultsLabelInfoText)
-        #self.layout.addWidget(self.resultLabel)
+        self.subLayout3h.addWidget(self.resultsLabelInfoText)
+        # self.layout.addWidget(self.resultLabel)
         self.resultLabel.setMinimumHeight(int(screen.availableSize().height() * 0.1))
 
         self.scrollArea = QScrollArea(self)
